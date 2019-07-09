d6d058f0945ed85fef953fe87c94d8b2b1304c22
shanejonas <jonas.shane@gmail.com>
2019-07-08 17:47:37 -0700

    fix(semantic-release): add verifyconditions and use NPM_TOKEN

--- publish.sh
+++ publish.sh
@@ -1,6 +1,5 @@
-env
 npm run generator-client
-
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > build/typescript/.npmrc
 cd build/typescript && npm publish --access=public
 cd -
 cd build/rust && cargo package && cargo publish --token $CARGO_TOKEN
